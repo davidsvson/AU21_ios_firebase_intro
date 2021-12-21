0cc9b699cf06bb155ebc3642879b3f5ff81c9257
David Svensson <davidsvson@gmail.com>
2021-12-21 09:10:17 +0100

    Initial Commit

---
+++ Firebase-Intro/Firebase_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_IntroApp.swift
+//  Firebase-Intro
+//
+//  Created by David Svensson on 2021-12-21.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
